268be5c6879970980fa8be731760b9a6b0b27eaf
Mohamed Elsaid <m.saeed2003@hotmail.com>
2025-10-09 18:35:03 +0300

    added event loop to response on each ping

--- app/main.go
+++ app/main.go
@@ -17,10 +17,16 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		buffer := make([]byte, 1024)
+		conn.Read(buffer)
+		fmt.Println(string(buffer))
+		conn.Write([]byte("+PONG\r\n"))
 	}
-	conn.Write([]byte("+PONG\r\n"))
 }
