027585037670c7602bb487731f5f454aa519c6d2
Mohamed Elsaid <m.saeed2003@hotmail.com>
2025-10-11 18:36:06 +0300

    adding expiry time with set commands

--- app/main.go
+++ app/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
+	"time"
 )
 
 const (
@@ -16,8 +18,6 @@ const (
 )
 
 type commands []string
-
-var data = map[string]string{}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -46,6 +46,7 @@ func main() {
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 
+	var data = &sync.Map{}
 	for {
 		buffer := make([]byte, 128)
 
@@ -61,41 +62,22 @@ func handleConn(conn net.Conn) {
 		if buf[0][0] == '*' {
 			// this should be an array
 			cmds := parse(buf)
-			switch cmds[0] {
-			case ping:
-				if len(cmds) == 1 {
-					conn.Write([]byte("+PONG\r\n"))
-				} else {
+			if len(cmds) > 0 {
+				switch cmds[0] {
+				case ping:
+					handlePing(cmds, conn)
+				case echo:
+					handleEcho(cmds, conn)
+				case set:
+					handleSet(cmds, data, conn)
+				case get:
+					handleGet(cmds, data, conn)
+				default:
 					conn.Write([]byte("+ERROR\r\n"))
+					continue
 				}
-			case echo:
-				if len(cmds) > 1 && len(cmds) < 3 {
-					arg := cmds[1]
-					response := fmt.Sprintf("$%d\r\n%s\r\n", len(arg), arg)
-					conn.Write([]byte(response))
-				} else {
-					conn.Write([]byte("+ERROR\r\n"))
-				}
-			case set:
-				if len(cmds) == 3 {
-					data[cmds[1]] = cmds[2]
-					conn.Write([]byte("+OK\r\n"))
-				} else {
-					conn.Write([]byte("+ERROR\r\n"))
-				}
-			case get:
-				if len(cmds) == 2 {
-					if val, ok := data[cmds[1]]; ok {
-						response := fmt.Sprintf("+%s\r\n", val)
-						conn.Write([]byte(response))
-					} else {
-						response := "+(nil)\r\n"
-						conn.Write([]byte(response))
-					}
-				}
-			default:
+			} else {
 				conn.Write([]byte("+ERROR\r\n"))
-				continue
 			}
 		} else {
 			conn.Write([]byte("+ERROR\r\n"))
@@ -108,9 +90,71 @@ func parse(buf [][]byte) commands {
 	cmds := make(commands, 0, 10)
 	for _, val := range buf {
 		val = bytes.ToLower(val)
-		if val[0] >= 'a' && val[0] <= 'z' {
+		if (val[0] >= 'a' && val[0] <= 'z') || (val[0] >= '0' && val[0] <= '9') {
 			cmds = append(cmds, string(val))
 		}
 	}
 	return cmds
+}
+
+func handlePing(cmds commands, conn net.Conn) {
+	if len(cmds) == 1 {
+		conn.Write([]byte("+PONG\r\n"))
+	} else {
+		conn.Write([]byte("+ERROR\r\n"))
+	}
+}
+
+func handleEcho(cmds commands, conn net.Conn) {
+	if len(cmds) > 1 && len(cmds) < 3 {
+		arg := cmds[1]
+		response := fmt.Sprintf("$%d\r\n%s\r\n", len(arg), arg)
+		conn.Write([]byte(response))
+	} else {
+		conn.Write([]byte("+ERROR\r\n"))
+	}
+}
+
+func handleSet(cmds commands, data *sync.Map, conn net.Conn) {
+	fmt.Println(len(cmds))
+	if len(cmds) == 3 {
+		key, value := cmds[1], cmds[2]
+		data.Store(key, value)
+		conn.Write([]byte("+OK\r\n"))
+	} else if len(cmds) == 5 {
+		key, value, expiry, t := cmds[1], cmds[2], cmds[3], cmds[4]
+
+		data.Store(key, value)
+
+		switch expiry {
+		case "ex":
+			t += "s"
+		case "px":
+			t += "ms"
+		}
+		newTime, err := time.ParseDuration(t)
+		if err != nil {
+			conn.Write([]byte("Error invalid time"))
+			return
+		}
+		go func() {
+			time.Sleep(newTime)
+			data.Delete(key)
+		}()
+		conn.Write([]byte("+OK\r\n"))
+	} else {
+		conn.Write([]byte("+ERROR\r\n"))
+	}
+}
+
+func handleGet(cmds commands, data *sync.Map, conn net.Conn) {
+	if len(cmds) == 2 {
+		if val, ok := data.Load(cmds[1]); ok {
+			response := fmt.Sprintf("+%s\r\n", val)
+			conn.Write([]byte(response))
+		} else {
+			response := "$-1\r\n"
+			conn.Write([]byte(response))
+		}
+	}
 }
