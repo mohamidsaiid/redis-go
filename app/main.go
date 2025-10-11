ec3698c230a894fabe92fc09f816d18cf805270f
Mohamed Elsaid <m.saeed2003@hotmail.com>
2025-10-11 04:16:46 +0300

    added the echo command

--- app/main.go
+++ app/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
 )
+
+var (
+	echo    = "echo"
+	ping    = "ping"
+	command = "command"
+	docs    = "docs"
+	sep     = []byte("\r\n")
+)
+
+type commands []string
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -39,11 +50,41 @@ func handleConn(conn net.Conn) {
 		_, err := conn.Read(buffer)
 		if err != nil {
 			fmt.Println(err)
-		}
-		_, err = conn.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			fmt.Println(err)
+			conn.Write([]byte(err.Error()))
 			break
 		}
+
+		buf := bytes.Split(buffer, []byte("\r\n"))
+		if buf[0][0] == '*' {
+			// this should be an array
+			cmds := parse(buf)
+			switch cmds[0] {
+			case ping:
+				conn.Write([]byte("+PONG\r\n"))
+			case echo:
+				if len(cmds) > 1 {
+					arg := cmds[1]
+					response := fmt.Sprintf("$%d\r\n%s\r\n", len(arg), arg)
+					conn.Write([]byte(response))
+				}
+			default:
+				conn.Write([]byte("+ERROR\r\n"))
+				continue
+			}
+		} else {
+			conn.Write([]byte("+ERROR\r\n"))
+		}
+
 	}
+}
+
+func parse(buf [][]byte) commands {
+	cmds := make(commands, 0, 10)
+	for _, val := range buf {
+		val = bytes.ToLower(val)
+		if val[0] >= 'a' && val[0] <= 'z' {
+			cmds = append(cmds, string(val))
+		}
+	}
+	return cmds
 }
