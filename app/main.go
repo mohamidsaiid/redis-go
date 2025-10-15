d75e9e373718b57261d6d0ed6c5fc47746411478
Mohamed Elsaid <m.saeed2003@hotmail.com>
2025-10-15 16:02:07 +0300

    fixing negative indexing lrange

--- app/main.go
+++ app/main.go
@@ -209,26 +209,27 @@ func LRangeBuilder(list []string, start, end int) []byte {
 		str.WriteString("*0\r\n")
 		return []byte(str.String())
 	}
+
 	if end >= len(list) {
 		end = len(list) - 1
 	}
-	if end * -1 > len(list) {
+	if end*-1 > len(list) {
 		end = 0
 	} else if end < 0 {
 		end += len(list)
 	}
-	if start * -1 > len(list) {
+	if start*-1 > len(list) {
 		start = 0
 	} else if start < 0 {
 		start += len(list)
 	}
+	str.WriteString(fmt.Sprintf("*%d\r\n", end-start+1))
 	for i := start; i <= end; i++ {
 		s := fmt.Sprintf("$%d\r\n%s\r\n", len(list[i]), list[i])
 		str.WriteString(s)
 	}
 
 	fmt.Println(start, end)
-	str.WriteString(fmt.Sprintf("*%d\r\n", end-start+1))
 
 	fmt.Println(str.String())
 	return []byte(str.String())
